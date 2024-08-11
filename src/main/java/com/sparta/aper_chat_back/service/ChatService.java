c423740dbd75b5e2628e99fca94065f8affd0f19
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-08-11 14:25:34 +0900

    feat: saveMessage, getChatHistory, saveAndBroadcastMessage service code created

--- src/main/java/com/sparta/aper_chat_back/service/ChatService.java
+++ src/main/java/com/sparta/aper_chat_back/service/ChatService.java
@@ -2,22 +2,46 @@ package com.sparta.aper_chat_back.service;
 
 import com.sparta.aper_chat_back.dto.MessageRequestDto;
 import com.sparta.aper_chat_back.entity.ChatMessage;
-import org.apache.logging.log4j.message.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.publisher.Sinks;
 
 @Service
 public class ChatService {
 
-    @Autowired
-    private ReactiveMongoTemplate reactiveMongoTemplate;
+    private final ReactiveMongoTemplate reactiveMongoTemplate;
+    private final SimpMessagingTemplate messagingTemplate;
 
-    public Mono<Void> saveMessage(MessageRequestDto requestDto) {
-        String collectionName = "chatRoom_" + requestDto.getChatRoomId();
+    public ChatService(ReactiveMongoTemplate reactiveMongoTemplate, SimpMessagingTemplate messagingTemplate) {
+        this.reactiveMongoTemplate = reactiveMongoTemplate;
+        this.messagingTemplate = messagingTemplate;
+    }
+
+    @Transactional
+    public Mono<ChatMessage> saveMessage(MessageRequestDto requestDto) {
+        String collectionName = "chat_" + requestDto.getChatRoomId();
         ChatMessage chatMessage = new ChatMessage(requestDto);
+        return reactiveMongoTemplate.save(chatMessage, collectionName)
+                .thenReturn(chatMessage);
+    }
 
-        return reactiveMongoTemplate.save(chatMessage, collectionName).then();
+    @Transactional
+    public Flux<ChatMessage> getChatHistory(Long roomId) {
+        String collectionName = "chat_" + roomId;
+        return reactiveMongoTemplate.findAll(ChatMessage.class, collectionName);
+    }
+
+
+    @Transactional
+    public Mono<ChatMessage> saveAndBroadcastMessage(MessageRequestDto requestDto) {
+        return saveMessage(requestDto)
+                .doOnSuccess(savedMessage -> {
+                    messagingTemplate.convertAndSend("/subscribe/" + requestDto.getChatRoomId(), savedMessage);
+                });
     }
 }
