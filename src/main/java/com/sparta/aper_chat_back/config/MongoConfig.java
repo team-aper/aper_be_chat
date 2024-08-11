4502134bf0b59849839c0d8840d3b870d260de18
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-08-11 22:38:47 +0900

    feat: to change uri depends on environment

--- src/main/java/com/sparta/aper_chat_back/config/MongoConfig.java
+++ src/main/java/com/sparta/aper_chat_back/config/MongoConfig.java
@@ -1,6 +1,7 @@
 package com.sparta.aper_chat_back.config;
 
 import com.mongodb.reactivestreams.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
@@ -8,8 +9,11 @@ import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 @Configuration
 public class MongoConfig {
 
+    @Value("${mongodb.uri}")
+    private String mongoUri;
+
     @Bean
     public ReactiveMongoTemplate reactiveMongoTemplate() {
-        return new ReactiveMongoTemplate(MongoClients.create("mongodb://localhost:27017"), "aper");
+        return new ReactiveMongoTemplate(MongoClients.create(mongoUri), "aper");
     }
 }
