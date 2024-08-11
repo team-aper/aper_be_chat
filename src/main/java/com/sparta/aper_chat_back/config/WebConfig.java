f5529c399e60da3e494731cb5686054fd1ebe18a
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-08-11 14:24:11 +0900

    docs: thread feat comment out

--- src/main/java/com/sparta/aper_chat_back/config/WebConfig.java
+++ src/main/java/com/sparta/aper_chat_back/config/WebConfig.java
@@ -1,18 +1,29 @@
 //package com.sparta.aper_chat_back.config;
 //
+//import org.springframework.context.annotation.Bean;
 //import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.core.task.AsyncTaskExecutor;
+//import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+//import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
 //import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 //
 //@Configuration
 //public class WebConfig implements WebMvcConfigurer {
 //
+//    @Bean(name = "mvcTaskExecutor")
+//    public AsyncTaskExecutor taskExecutor() {
+//        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+//        executor.setCorePoolSize(10);  // 기본적으로 유지되는 스레드의 수
+//        executor.setMaxPoolSize(50);   // 최대 생성될 수 있는 스레드의 수
+//        executor.setQueueCapacity(100); // 스레드 풀에서 처리할 수 있는 대기 요청 수
+//        executor.setThreadNamePrefix("MvcAsync-"); // 생성되는 스레드의 이름 접두사
+//        executor.initialize();
+//        return executor;
+//    }
+//
 //    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOriginPatterns("http://localhost:8081")  // Specify the allowed origin
-//                .allowedMethods("GET", "POST", "PUT", "DELETE")  // Specify allowed HTTP methods
-//                .allowedHeaders("*")  // Allow all headers
-//                .allowCredentials(true);  // Allow credentials
+//    public void configureAsyncSupport(AsyncSupportConfigurer configurer) {
+//        configurer.setTaskExecutor(taskExecutor());
+//        configurer.setDefaultTimeout(30000); // 비동기 요청의 기본 타임아웃 (30초)
 //    }
 //}
