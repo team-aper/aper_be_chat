44797c49d2674e35b4f8e017cc6b8013dc72addf
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-08-11 15:18:09 +0900

    feat: swagger config

---
+++ src/main/java/com/sparta/aper_chat_back/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.sparta.aper_chat_back.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Chat Service API")
+                .description("This API provides endpoints for managing chat")
+                .version("1.0.0");
+    }
+}
