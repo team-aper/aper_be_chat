b2ea4a559ba0ced9309670c66385b7f5a0aa4c62
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-10-09 16:35:06 +0900

    feat: to specify db type

--- src/main/java/com/sparta/aper_chat_back/global/security/user/respository/UserRepository.java
+++ src/main/java/com/sparta/aper_chat_back/global/security/user/respository/UserRepository.java
@@ -5,10 +5,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.util.List;
 import java.util.Optional;
 
+@EnableJpaRepositories
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
 
