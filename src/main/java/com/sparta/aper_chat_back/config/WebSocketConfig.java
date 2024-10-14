8dda7b22bccc0a9e2add88379a03e860c635feb0
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-10-14 21:56:41 +0900

    feat: for messaging queue

--- src/main/java/com/sparta/aper_chat_back/config/WebSocketConfig.java
+++ src/main/java/com/sparta/aper_chat_back/config/WebSocketConfig.java
@@ -2,6 +2,7 @@ package com.sparta.aper_chat_back.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -12,7 +13,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/subscribe");
+        registry.setPathMatcher(new AntPathMatcher("."));
+        registry.setApplicationDestinationPrefixes("/pub");
+
+        registry.enableStompBrokerRelay("/queue", "/topic", "/exchange", "/amq/queue");
     }
 
     @Override
