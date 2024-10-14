7a50bd388e70098642d4fba73a20e64f1df5972f
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-10-14 22:11:38 +0900

    feat: rabbit config init commit

---
+++ src/main/java/com/sparta/aper_chat_back/config/RabbitConfig.java
@@ -0,0 +1,84 @@
+package com.sparta.aper_chat_back.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableRabbit
+public class RabbitConfig {
+
+    @Value("${local.rabbitmq.username}")
+    private String rabbitMqUsername;
+
+    @Value("${local.rabbitmq.password}")
+    private String rabbitMqPassword;
+    private static final String CHAT_QUEUE_NAME = "chat.queue";
+    private static final String CHAT_EXCHANGE_NAME = "chat.exchange";
+    private static final String ROUTING_KEY = "room.*";
+
+    @Bean
+    public Queue queue() {
+        return new Queue(CHAT_QUEUE_NAME, true);
+    }
+
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(CHAT_EXCHANGE_NAME);
+    }
+
+    @Bean
+    public Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate() {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory());
+        rabbitTemplate.setMessageConverter(jsonMessageConverter());
+        rabbitTemplate.setRoutingKey(CHAT_QUEUE_NAME);
+        return rabbitTemplate;
+    }
+
+    @Bean
+    public SimpleMessageListenerContainer container() {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory());
+        container.setQueueNames(CHAT_QUEUE_NAME);
+        container.setMessageListener(null);
+        return container;
+    }
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory factory = new CachingConnectionFactory();
+        factory.setHost("localhost");
+        factory.setUsername(rabbitMqUsername);
+        factory.setPassword(rabbitMqPassword);
+        return factory;
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter jsonMessageConverter() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, true);
+        objectMapper.registerModule(dateTimeModule());
+
+        return new Jackson2JsonMessageConverter(objectMapper);
+    }
+
+    @Bean
+    public JavaTimeModule dateTimeModule() {
+        return new JavaTimeModule();
+    }
+}
