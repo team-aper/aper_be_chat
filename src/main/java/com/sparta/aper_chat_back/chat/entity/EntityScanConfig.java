7dbddcefc9e4f780d2da75467cc347288658c1b6
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-11-22 13:22:21 +0900

    feat: adding entityScanConfig

---
+++ src/main/java/com/sparta/aper_chat_back/chat/entity/EntityScanConfig.java
@@ -0,0 +1,9 @@
+package com.sparta.aper_chat_back.chat.entity;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EntityScan(basePackages = "com.aperlibrary.chat.entity")
+public class EntityScanConfig {
+}
